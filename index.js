6bed77b6615d301dd5f3306340f4958ddd892de1
Hervé Guétin <herve.guetin@agence-soon.fr>
2015-05-01 23:15:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Hervé Guétin's blog
+ *
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author Hervé Guétin <@herveguetin> <herve.guetin@gmail.com>
+ * @category Blog
+ * @copyright Copyright (c) 2015 Hervé Guétin (http://www.herveguetin.com)
+ */
+
+var express = require('express');
+var app = express();
+
+app.use('/', function(req, res) {
+    res.send('Hello World again');
+});
+
+app.listen(3000);
+console.log('Server running on port 3000');
+
+module.exports = app;
